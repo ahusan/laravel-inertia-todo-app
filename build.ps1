b5f68deb13d14d323cfa6714f81cb8e44714c9f6
ahusan <email@ahusan.dev>
2024-02-13 01:19:30 +0500

    changes to folder structure and build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,23 @@
 # This is a PowerShell script
 
+# this is now redundant as we are building it in the dockerfile
+# npm run build
+# if($LASTEXITCODE -ne 0) {
+#     Write-Host "NPM Build failed" -ForegroundColor Red
+#     exit 1
+# }
+
+Write-Host 'Build & Up latest' -ForegroundColor Cyan
+docker compose up -d --build
+if( $LASTEXITCODE -ne 0 ){
+    Write-Host "Docker Compose failed" -ForegroundColor Red
+    exit 1
+}
+docker compose exec -it php-fpm php artisan migrate:fresh
+if( $LASTEXITCODE -ne 0 ){
+    Write-Host "PHP artisan migration failed" -ForegroundColor Red
+    exit 1
+}
 # Run your command
 Write-Host 'Running tests' -ForegroundColor Cyan
 $output = & php artisan test | Tee-Object -FilePath testoutput.txt
@@ -18,13 +36,3 @@ if ($output -match "failed" -or $output -match "fail") {
     Write-Host "Passed tests: $($testSummary[1])" -ForegroundColor Green
     Write-Host "Skipped tests: $($testSummary[0])" -ForegroundColor Yellow
 }
-
-# this is now redundant as we are building it in the dockerfile
-# npm run build
-# if($LASTEXITCODE -ne 0) {
-#     Write-Host "NPM Build failed" -ForegroundColor Red
-#     exit 1
-# }
-
-Write-Host 'Build & Up latest' -ForegroundColor Cyan
-# docker compose up -d --build
--- build.sh
+++ build.sh
@@ -1,24 +1,5 @@
  #!/bin/bash
 
-# Run your command
-echo -e "\033[36mRunning tests\033[0m"
-output=$(php artisan test | tee testoutput.txt)
-
-# Extract the number of passed and skipped tests
-skipped_tests=$(echo "$output" | grep -oP 'Tests: +\K[0-9]+(?= skipped)')
-passed_tests=$(echo "$output" | grep -oP 'Tests: +\K[0-9]+(?= passed)')
-failed_tests=$(echo "$output" | grep -oP 'Tests: +\K[0-9]+(?= fail)')
-
-# Check if the output contains "failed"
-if [[ $output == *"fail"* ]]; then
-    echo -e  "\033[31mTest failed\033[0m"
-    echo -e  "\033[31mFailed tests: $failed_tests\033[0m"
-else
-    echo -e  "\033[32mTest passed\033[0m"
-    echo -e  "\033[32mPassed tests: $passed_tests\033[0m"
-    echo -e  "\033[32mPassed tests: $skipped_tests\033[0m"
-fi
-
 # Build Vite for production
 # this is now redundant as we are building it in the dockerfile
 # echo -e  "\033[36mBuilding Vite for production\033[0m"
@@ -30,7 +11,15 @@ fi
 
 echo -e  "\033[36mBuild & Up latest\033[0m"
 docker compose up -d --build
+if [ $? -ne 0 ]; then
+    echo -e  "\033[31mDocker compose failed.\033[0m"
+    exit 1
+fi
 docker exec -it php-fpm php artisan migrate
+if [ $? -ne 0 ]; then
+    echo -e  "\033[31mFailed to migrate\033[0m"
+    exit 1
+fi
 # Run your command
 echo -e "\033[36mRunning tests\033[0m"
 output=$(docker exec -it php-fpm php artisan test | tee testoutput.txt)
